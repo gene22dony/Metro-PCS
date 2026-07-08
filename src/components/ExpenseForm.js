22a199114efce1c9fa0e37ab09c503e01ca25d33
gene22dony <gene22dony@outlook.com>
2026-07-08 13:59:24 +0530

    Create ExpenseForm.js

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,11 @@
+function ExpenseForm(){
+ return (
+  <div>
+   <input placeholder="Expense Name"/>
+   <input placeholder="Amount"/>
+   <button>Add Expense</button>
+  </div>
+ )
+}
+
+export default ExpenseForm;
