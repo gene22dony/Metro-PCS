58a43cca6282aff71cb76e91cdbe840aa7866daa
gene22dony <gene22dony@outlook.com>
2026-07-08 13:56:21 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ExpenseForm />
+      <ExpenseList />
+    </div>
+  );
+}
+
+export default App;
